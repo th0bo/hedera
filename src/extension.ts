bfa0935626298c279608a872e5547be83a135d8c
Théo Boulaire <th.boulaire@gmail.com>
2023-12-30 21:31:38 +0100

    Show correction basic command

--- src/extension.ts
+++ src/extension.ts
@@ -14,23 +14,19 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const outputChannel = vscode.window.createOutputChannel("Hedera");
+  const baseDir = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath as string;
+  const options: Partial<SimpleGitOptions> = {
+    baseDir,
+    binary: "git",
+    maxConcurrentProcesses: 6,
+    trimmed: false,
+  };
+  const git = simpleGit(options);
   const disposables = [
     vscode.commands.registerCommand("hedera.test", async () => {}),
     vscode.commands.registerCommand("hedera.startExercise", async () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      const baseDir = vscode.workspace.workspaceFolders?.[0]?.uri
-        .fsPath as string;
-      const options: Partial<SimpleGitOptions> = {
-        baseDir,
-        binary: "git",
-        maxConcurrentProcesses: 6,
-        trimmed: false,
-      };
-      const git = simpleGit(options);
-      const diffResult = await git.diff();
-      outputChannel.appendLine(diffResult);
-      outputChannel.show();
       const exercises: vscode.QuickPickItem[] = (await git.tags()).all.map(
         (tag) => ({ label: tag })
       );
@@ -50,9 +46,18 @@ export function activate(context: vscode.ExtensionContext) {
         await git.commit("Hedera");
       }
       await git.checkout(pickedExercise?.label as string, (err, data) => {});
-      vscode.window.showInformationMessage(`Starting exercise ${pickedExercise}.`);
+      vscode.window.showInformationMessage(
+        `Starting exercise ${pickedExercise}.`
+      );
     }),
-    vscode.commands.registerCommand("hedera.showCorrection", () => {}),
+    vscode.commands.registerCommand("hedera.showCorrection", async () => {
+      const diffResult = await git.diff(["test0..test1"]);
+      const diffDocument = await vscode.workspace.openTextDocument({
+        content: diffResult,
+        language: "diff",
+      });
+      vscode.window.showTextDocument(diffDocument);
+    }),
   ];
 
   context.subscriptions.push(...disposables);
