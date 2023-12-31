d38e61116db7cd77b993ed6631e16a790b62759e
Théo Boulaire <th.boulaire@gmail.com>
2023-12-31 11:28:57 +0100

    Multiple folders workspace

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import { simpleGit, SimpleGitOptions } from "simple-git";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "hedera" is now active!');
@@ -14,7 +14,17 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const outputChannel = vscode.window.createOutputChannel("Hedera");
-  const baseDir = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath as string;
+  
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (workspaceFolders === undefined || workspaceFolders.length === 0) {
+    throw new Error("No workspace folder.");
+  }
+  const baseDir = (
+    workspaceFolders.length > 1
+      ? ((await vscode.window.showWorkspaceFolderPick()) as vscode.WorkspaceFolder)
+      : workspaceFolders[0]
+  ).uri.fsPath;
+
   const options: Partial<SimpleGitOptions> = {
     baseDir,
     binary: "git",
