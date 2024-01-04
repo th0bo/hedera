1048400d7117462207d80a25a9ccead2aff84c4b
Théo Boulaire <th.boulaire@gmail.com>
2024-01-04 13:19:17 +0100

    Using cwd instead of cd prefixes

--- src/git.ts
+++ src/git.ts
@@ -1,26 +1,23 @@
 import * as cp from "child_process";
 
 export class Git {
-  private readonly cdCmd: string;
-  constructor(private readonly baseDir: string) {
-    this.cdCmd = `cd ${this.baseDir} && `;
-  }
+  constructor(private readonly baseDir: string) {}
 
   diff = (revisionA: string, revisionB: string, fileRelativePath?: string) => {
     const filesOption =
       fileRelativePath === undefined ? "" : ` -- ${fileRelativePath}`;
-    const cmd = this.cdCmd + `git diff ${revisionA}..${revisionB}${filesOption}`;
-    return cp.execSync(cmd).toString();
+    const cmd = `git diff ${revisionA}..${revisionB}${filesOption}`;
+    return cp.execSync(cmd, { cwd: this.baseDir }).toString();
   };
 
   tag = () => {
-    const cmd = this.cdCmd + `git tag`;
-    return cp.execSync(cmd).toString().split("\n").filter(tag => tag !== "");
+    const cmd = `git tag`;
+    return cp.execSync(cmd, { cwd: this.baseDir }).toString().split("\n").filter(tag => tag !== "");
   };
 
   status = () => {
-    const cmd = this.cdCmd + `git status`;
-    const output = cp.execSync(cmd).toString();
+    const cmd = `git status`;
+    const output = cp.execSync(cmd, { cwd: this.baseDir }).toString();
     const detachedHeadMatcher = output.match(/HEAD detached at (\w+)/);
     return {
       clean: output.includes("nothing to commit, working tree clean"),
@@ -30,23 +27,23 @@ export class Git {
   };
 
   add = () => {
-    const cmd = this.cdCmd + `git add -A`;
-    return cp.execSync(cmd).toString();
+    const cmd = `git add -A`;
+    return cp.execSync(cmd, { cwd: this.baseDir }).toString();
   };
 
   commit = (message: string) => {
-    const cmd = this.cdCmd + `git commit -m '${message}'`;
-    return cp.execSync(cmd).toString();
+    const cmd = `git commit -m '${message}'`;
+    return cp.execSync(cmd, { cwd: this.baseDir }).toString();
   };
 
   checkout = (revision: string) => {
-    const cmd = this.cdCmd + `git checkout ${revision}`;
-    return cp.execSync(cmd).toString();
+    const cmd = `git checkout ${revision}`;
+    return cp.execSync(cmd, { cwd: this.baseDir }).toString();
   };
 
   switch = (branch: string, create: boolean = false) => {
     const createOption = create ? " -c" :  "";
-    const cmd = this.cdCmd + `git switch${createOption} ${branch}`;
-    return cp.execSync(cmd).toString();
+    const cmd = `git switch${createOption} ${branch}`;
+    return cp.execSync(cmd, { cwd: this.baseDir }).toString();
   };
 }
